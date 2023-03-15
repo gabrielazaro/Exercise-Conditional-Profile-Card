f0bca6b97633590782ce5d6380e9fbff61bfea06
Gabriela Zambrano Rodríguez <gabrielazaro97@gmail.com>
2023-03-15 19:43:02 +0000

    modificaciones ejercicio

--- src/js/app.js
+++ src/js/app.js
@@ -33,17 +33,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name || "Name"} ${variables.lastname ||
+    "Lastname"} </h1>
+          <h2>${variables.role || "Role"}</h2>
+          <h3>${variables.city || "City"} ${variables.country || "Country"}</h3>
+          <ul class= ${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fa-brands fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fa-brands fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }"><i class="fa-brands fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fa-brands fa-instagram"></i></a></li>
           </ul>
         </div>
-    `;
+        `;
 }
 
 /**
@@ -58,7 +67,7 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "positon-left",
     // social media usernames
     twitter: null,
     github: "alesanchezr",
